69b47d1de9c853498ce6febc557d3e5bfe8b6d2e
bleanovaya <lina.otherside666@gmail.com>
2025-08-29 05:04:48 +0300

    fix: finalize check function logic to pass all tests and meet eslint rules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+
+  const openBrackets = [];
+  const bracketPairs = {};
+
+  bracketsConfig.forEach(([open, close]) => {
+    openBrackets.push(open);
+    bracketPairs[close] = open;
+  });
+
+  str.split('').forEach((char) => {
+    if (openBrackets.includes(char)) {
+      if (bracketPairs[char] === char) {
+        if (stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else {
+      const last = stack[stack.length - 1];
+      if (bracketPairs[char] === last) {
+        stack.pop();
+      } else {
+        stack.push('invalid');
+      }
+    }
+  });
+
+  return stack.length === 0;
 };
